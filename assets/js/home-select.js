e5f91a8c0eb263e24014c9a137967a9b6780da99
Daniel Aguilar <dado88@dadoMac.local>
2023-02-14 22:22:06 -0800

    "currentyear" now updates automatically, and some basic comments are added

--- assets/js/home-select.js
+++ assets/js/home-select.js
@@ -1,15 +1,20 @@
 years = document.getElementById('sub-nav').children
 decades = ['196','197','198','199','200','201','202']
-currentyear = '2022'
+//below gets the current school year, offset by about 7 months to match up with the hall of fame
+currentyear = new Date(new Date().getFullYear(), new Date().getMonth() - 7).getFullYear().toString()
 function select(s) {
+	// this loops for every year in the years array (variable "years")
 	for (let i = 0; i < years.length; i++) {
+		// checks if the current year being edited is the current year or a year preceding the current year, and if so proceeds to hide the element
 		if (decades[s] + i >= currentyear) {
 			years[i].innerHTML = '';
 			years[i].style.display = 'none';
 		}
+		// if the above check fails, then assume that the year is past and already posted on the website, and change the decade accordingly
 		else {
 			years[i].innerHTML = decades[s] + i;
 			years[i].style.display = 'block';
 		}
+		// i have a love-hate relationship with javascript
 	}
 }
